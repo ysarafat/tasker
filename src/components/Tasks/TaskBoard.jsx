3d4f7e4570dea6eddf97f13ccdcf392ae973950a
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-05-12 20:15:31 +0600

    Add AddTaskModal component; implement task creation functionality and integrate with TaskBoard

--- src/components/Tasks/TaskBoard.jsx
+++ src/components/Tasks/TaskBoard.jsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "I want to learn React such then i can treat it like my slave and make it do whatever I want to do",
+    tags: ["web", "react", "javascript"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowModal] = useState(false);
+  const handleAddTask = (task) => {
+    setTasks([...tasks, task]);
+    setShowModal(false);
+  };
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
-
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction />
-          <TaskList />
+          <TaskAction onAddClick={() => setShowModal(true)} />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
