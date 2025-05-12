b423cc5e1e742d42a2764d6474ac898e1a62b5e2
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-05-12 16:27:58 +0600

    Add TaskBoard, SearchTask, TaskAction, and TaskList components; restructure App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import TaskBoard from "./components/Tasks/TaskBoard";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Hero />
+      <div className="flex flex-col items-center justify-center">
+        <Hero />
+        <TaskBoard />
+      </div>
       <Footer />
     </>
   );
